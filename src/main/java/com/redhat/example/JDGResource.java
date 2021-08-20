ed4d435f00a2f2e9b430cf2c6228cc6dbf62a303
Claudio Luppi <cluppi@redhat.com>
2021-08-20 11:32:20 +0100

    support json+xml and fix http response code

--- src/main/java/com/redhat/example/JDGResource.java
+++ src/main/java/com/redhat/example/JDGResource.java
@@ -6,7 +6,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -18,16 +20,22 @@ public class JDGResource {
 
     @GET
     @Path("{cache-key}")
-    @Produces("text/plain")
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response getEntry(@PathParam("cache-key") String cacheKey) {
         String entry = jdgBean.get(cacheKey);
-        return Response.ok(entry).build();
+        if(entry == null){
+            return Response.status(Status.NO_CONTENT.getStatusCode(), "No entry for provided key: " + cacheKey).build();
+        } else {
+            return Response.ok(entry).build();
+        }
+        
     }
 
     @PUT
     @Path("{cache-key}")
-    @Consumes("text/plain")
-    public void putEntry(@PathParam("cache-key") String cacheKey, String cacheValue) {
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response putEntry(@PathParam("cache-key") String cacheKey, String cacheValue) {
         jdgBean.put(cacheKey, cacheValue);
+        return Response.status(Status.CREATED.getStatusCode(), "Entry successfully added to the cache with key: " + cacheKey).build();
     }
 }
